a8f302649ba236182a88a0e99ff9a1999844ec26
anton.chuprov <anton.chuprov@sperasoft.com>
2020-06-19 11:08:02 +0400

    Java Fundamentals Tasks

---
+++ SPERASOFT-practice/src/JavaFundamentals.java
@@ -0,0 +1,20 @@
+/*
+ *
+ * =======================================================================
+ *
+ * Copyright (c) 2009-2020 Sony Network Entertainment International, LLC. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Sony Network Entertainment International, LLC.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into with
+ * Sony Network Entertainment International, LLC.
+ *
+ * =======================================================================
+ *
+ * For more information, please see http://www.sony.com/SCA/outline/corporation.shtml
+ *
+ */
+
+public class JavaFundamentals {
+}
