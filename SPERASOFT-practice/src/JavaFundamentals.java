7eb4a0b4fedcef72edf8a063f0f5b983ada1a1a0
anton.chuprov <anton.chuprov@sperasoft.com>
2020-06-19 17:07:45 +0400

    #1, 2, 3

--- SPERASOFT-practice/src/JavaFundamentals.java
+++ SPERASOFT-practice/src/JavaFundamentals.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
 
  * Java Fundamentals Practice #1: Find the minimum
@@ -42,10 +46,15 @@
 
 public class JavaFundamentals {
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
         int ant = fourArgs(222, 51, 111, 313);
         System.out.println(ant);
+        
+        arrayGenerator();
+        arraySort();
     }
+
+    /* Practice #1 */
 
     public static int twoArgs(int arg1, int arg2) {
         return arg1 < arg2 ? arg1 : arg2;
@@ -61,4 +70,80 @@ public class JavaFundamentals {
         return arg < arg4 ? arg : arg4;
     }
 
+    /* Practice #2 */
+
+    public static void arrayGenerator() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader( System.in ) );
+        int size = Integer.parseInt(bufferedReader.readLine());
+        int index = Integer.parseInt(bufferedReader.readLine());
+
+        int arraj[] = new int[size];
+
+        for ( int i = 0; i < arraj.length; i++ ) {
+            arraj[i] = (int) Math.round(Math.random() * 999);
+        }
+
+        for ( Integer j : arraj ) {
+            if (j == index) {
+                System.out.println("true");
+                break;
+            } else {
+                System.out.println("false");
+                break;
+            }
+        }
+    }
+
+    /* Practice #3 */
+
+    public static void arraySort() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader( System.in ) );
+        int size1 = Integer.parseInt(bufferedReader.readLine());
+        int size2 = Integer.parseInt(bufferedReader.readLine());
+
+        int arr1[] = new int[size1];
+        int arr2[] = new int[size2];
+
+        for ( int i = 0; i < arr1.length; i++ ) {
+            arr1[i] = (int) Math.round(Math.random() * 999);
+        }
+
+        for ( int j = 0; j < arr2.length; j++ ) {
+            arr2[j] = (int) Math.round(Math.random() * 999);
+        }
+
+        //bubble sort arr1
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1.length - i - 1; j++) {
+                if ( arr1[j] > arr1[j + 1]) {
+                    int temp = arr1[j];
+                    arr1[j] = arr1[j + 1];
+                    arr1[j + 1] = temp;
+                }
+            }
+        }
+
+        //insertion sort arr2
+        for ( int i = 1; i < arr2.length; i++ ) {
+            int current = arr2[i];
+            int j = i - 1;
+
+            while( j >= 0 && current < arr2[j] ) {
+                arr2[j + 1] = arr2[j];
+                j--;
+            }
+
+            arr2[j+1] = current;
+        }
+
+        for (Integer i : arr1) {
+            System.out.print(i + ", ");
+        }
+
+        System.out.println();
+
+        for (Integer j : arr2) {
+            System.out.print(j + ", ");
+        }
+    }
 }
